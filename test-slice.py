dec94b7b9f2ea61ddaaa9ea28f04364b13948086
Ben Challenor <ben@challenor.org>
2013-11-09 01:27:39 +0000

    Rename slice->edit, pre->0, post->1 so test cases line up

--- test-slice.py
+++ test-slice.py
@@ -21,22 +21,22 @@ class VirtualTempDir:
 
 class VirtualTodoEnv(unittest.TestCase):
   todo_file_name = "todo.txt"
-  __slice_dir_path = "SLICE"
-  __slice_file_path = os.path.join(__slice_dir_path, todo_file_name)
+  __edit_dir_path = "EDIT"
+  __edit_file_path = os.path.join(__edit_dir_path, todo_file_name)
   todo_dir_path = "TODO"
   todo_file_path = os.path.join(todo_dir_path, todo_file_name)
   today = date(2000, 1, 1)
 
-  def __init__(self, todo_pre, slice_pre, slice_post, todo_post, date_on_add, preserve_line_numbers, disable_filter):
+  def __init__(self, todo0, edit0, edit1, todo1, date_on_add, preserve_line_numbers, disable_filter):
     unittest.TestCase.__init__(self)
 
-    self.__todo_pre = todo_pre
-    self.__slice_pre = slice_pre
-    self.__slice_post = slice_post
-    self.__todo_post = todo_post
+    self.__todo0 = todo0
+    self.__edit0 = edit0
+    self.__edit1 = edit1
+    self.__todo1 = todo1
 
-    self.__slice_dir = VirtualTempDir(self.__slice_dir_path)
-    self.__slice_file_path_written = False
+    self.__edit_dir = VirtualTempDir(self.__edit_dir_path)
+    self.__edit_file_path_written = False
     self.__todo_file_path_written = False
 
     self.date_on_add = date_on_add
@@ -46,31 +46,31 @@ class VirtualTodoEnv(unittest.TestCase):
 
   def read_lines(self, path):
     if path == self.todo_file_path:
-      return self.__todo_pre
-    elif path == self.__slice_file_path:
-      return self.__slice_post
+      return self.__todo0
+    elif path == self.__edit_file_path:
+      return self.__edit1
     else:
       self.fail("attempt to read unknown path: %s" % path)
 
   def write_lines(self, path, lines):
     if path == self.todo_file_path:
-      self.assertEqual(self.__todo_post, lines, msg = "Todo file content does not match expected content")
+      self.assertEqual(self.__todo1, lines, msg = "Todo file content does not match expected content")
       self.__todo_file_path_written = True
-    elif path == self.__slice_file_path:
-      self.assertEqual(self.__slice_pre, lines, msg = "Slice file content does not match expected content")
-      self.__slice_file_path_written = True
+    elif path == self.__edit_file_path:
+      self.assertEqual(self.__edit0, lines, msg = "Slice file content does not match expected content")
+      self.__edit_file_path_written = True
     else:
       self.fail("attempt to write unknown path: %s" % path)
 
   def create_temp_dir(self):
-    return self.__slice_dir
+    return self.__edit_dir
 
   def launch_editor(self, path):
-    self.assertEqual(self.__slice_file_path, path)
+    self.assertEqual(self.__edit_file_path, path)
 
   def assert_success(self):
-    self.assertTrue(self.__slice_dir.clean_exit, msg = "Expected slice directory to be used and cleaned up")
-    self.assertTrue(self.__slice_file_path_written, msg = "Expected slice file to be written")
+    self.assertTrue(self.__edit_dir.clean_exit, msg = "Expected edit directory to be used and cleaned up")
+    self.assertTrue(self.__edit_file_path_written, msg = "Expected edit file to be written")
     self.assertTrue(self.__todo_file_path_written, msg = "Expected todo file to be written")
 
 
@@ -81,10 +81,10 @@ class SliceTest(unittest.TestCase):
       self,
       filter_args = [],
       expect_early_exit = False,
-      todo_pre = None,
-      slice_pre = None,
-      slice_post = None,
-      todo_post = None,
+      todo0 = None,
+      edit0 = None,
+      edit1 = None,
+      todo1 = None,
       date_on_add = False,
       preserve_line_numbers = True,
       disable_filter = False
@@ -100,10 +100,10 @@ class SliceTest(unittest.TestCase):
         args.extend(filter_args)
 
     env = VirtualTodoEnv(
-        todo_pre = todo_pre,
-        slice_pre = slice_pre,
-        slice_post = slice_post if slice_post is not None else slice_pre,
-        todo_post = todo_post if todo_post is not None else todo_pre,
+        todo0 = todo0,
+        edit0 = edit0,
+        edit1 = edit1 if edit1 is not None else edit0,
+        todo1 = todo1 if todo1 is not None else todo0,
         date_on_add = date_on_add,
         preserve_line_numbers = preserve_line_numbers,
         disable_filter = disable_filter
@@ -120,22 +120,22 @@ class SliceMatchTest(SliceTest):
 
   def test_single_task(self):
     self.run_test(
-        todo_pre = ["task"],
-        slice_pre = ["i:1 task"]
+        todo0 = ["task"],
+        edit0 = ["i:1 task"]
         )
 
   def test_completed_tasks_are_hidden(self):
     self.run_test(
-        todo_pre = ["x 2000-01-01 done"],
-        slice_pre = []
+        todo0 = ["x 2000-01-01 done"],
+        edit0 = []
         )
 
   def test_empty_line_is_preserved(self):
     self.run_test(
-        todo_pre = ["", "orig"],
-        slice_pre = ["i:2 orig"],
-        slice_post = ["i:2 orig", "new"],
-        todo_post = ["", "orig", "new"],
+        todo0 = ["", "orig"],
+        edit0 = ["i:2 orig"],
+        edit1 = ["i:2 orig", "new"],
+        todo1 = ["", "orig", "new"],
         preserve_line_numbers = True
         )
 
